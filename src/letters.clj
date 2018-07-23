c5f68dcba2c4bc353f3d1b6757dc2e38ecd590e2
Michael Pendergrass <mdpendergrass@gmail.com>
2018-07-23 16:34:43 -0400

    try #12 to get working

--- src/letters.clj
+++ src/letters.clj
@@ -99,7 +99,7 @@
 
 (defn -main [& args]
 
-  (run-jetty #'handler {:port (or (System/getenv "PORT") 5000)})
+  (run-jetty #'handler {:port (Integer/parseInt (get (System/getenv) "PORT" "5000"))})
 
   (doto (net.dv8tion.jda.core.JDABuilder. net.dv8tion.jda.core.AccountType/BOT)
     (.setToken (get-token))
